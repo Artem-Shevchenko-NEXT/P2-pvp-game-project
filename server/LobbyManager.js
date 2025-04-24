7f47a1e2de25413d2d85112e407fad2308e8588c
StagnantBytes <artyom.shevchenko2005@gmail.com>
2025-04-24 11:25:03 +0200

    the 5 lines of code needed for making the defualt rrom logic, have to update the server.js as the next update and define some basic perematers like players and data needed to be gathered from players

--- server/LobbyManager.js
+++ server/LobbyManager.js
@@ -1,1 +1,15 @@
 // lobby management for multiplayer matches
+class LobbyManager {
+  constructor() {
+    // only has the defualt main room for now 
+    this.defaultRoom = 'main';
+  }
+  
+  // method for getting the  Get default room
+
+  getDefaultRoom() {
+    return this.defaultRoom;
+  }
+}
+
+module.exports = LobbyManager;
