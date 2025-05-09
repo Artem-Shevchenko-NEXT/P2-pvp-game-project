fa2526ae911a6e73d948cf2211754834021b82a9
StagnantBytes <artyom.shevchenko2005@gmail.com>
2025-05-09 13:18:28 +0200

    fixed the crashing

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -131,7 +131,6 @@ export class Game extends Phaser.Scene {
         } else if (this.selectedCharacter === 'skeleton') {
             this.player1 = new SkeletonCharacter(this, PLAYER1_SPAWN_X, PLAYER1_SPAWN_Y);
             this.player1.setScale(0.80);
-            this.player1.body.setDisplaySize(60,30);
         } else {
             // Fall back to tank as default
             this.player1 = new TankCharacter(this, PLAYER1_SPAWN_X, PLAYER1_SPAWN_Y);
